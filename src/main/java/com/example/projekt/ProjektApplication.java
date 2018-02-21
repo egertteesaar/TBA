ecd1dcb9a26eab80688d23fe35441b3b4d97069e
Johannes Laamann <jelaamann@gmail.com>
2018-02-21 16:16:55 +0200

    Initial commit

---
+++ src/main/java/com/example/projekt/ProjektApplication.java
@@ -0,0 +1,12 @@
+package com.example.projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektApplication.class, args);
+	}
+}
