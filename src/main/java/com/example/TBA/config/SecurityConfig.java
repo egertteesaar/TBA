ad52bfce68bc8539295cfa34d6c3dca1a85c3e31
Johannes Laamann <jelaamann@gmail.com>
2018-03-26 01:03:04 +0300

    Update security config

--- src/main/java/com/example/TBA/config/SecurityConfig.java
+++ src/main/java/com/example/TBA/config/SecurityConfig.java
@@ -1,26 +1,32 @@
 package com.example.TBA.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.savedrequest.NullRequestCache;
 
 @Configuration
-@EnableWebSecurity
-@ComponentScan
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/login*","/signin/**","/signup/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().loginPage("/login").permitAll()
-//                .and()
-//                .logout();
+        http
+                .authorizeRequests()
+                .antMatchers("/api/session").permitAll()
+                .antMatchers("/api/location/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .and()
+                .headers().frameOptions().disable() // for h2
+                .and()
+                .requestCache()
+                .requestCache(new NullRequestCache())
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and().csrf().disable();
     }
 
 }
