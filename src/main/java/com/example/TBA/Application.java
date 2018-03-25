418c0825e81603e1a108409f9c022ab3b726bf96
Johannes Laamann <jelaamann@gmail.com>
2018-03-25 23:22:44 +0300

    Downgrade spring boot

--- src/main/java/com/example/TBA/Application.java
+++ src/main/java/com/example/TBA/Application.java
@@ -5,12 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.stereotype.Component;
 
 
 @Component
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
