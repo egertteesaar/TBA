19fecc6a8d5491379192e2f98bbd5a301b4c86b1
Johannes Laamann <jelaamann@gmail.com>
2018-03-28 18:34:08 +0300

    Load data from backend, populate content with jQuery

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,72 +1,35 @@
-var lat;
-var long;
-
-function loadWeatherData(latitude, longitude) {
-    $.ajax({
-        url: "/api/location/" + latitude + "/" + longitude,
-        success: function(data) {
-            data = $.parseJSON(data);
-            clearIntroView();
-            loadResultView(data);
-        }
-    });
-}
-
-function loadWeatherDataByLocation(location) {
-    $.ajax({
-        url: "/api/location/" + location,
-        success: function(data) {
-            data = $.parseJSON(data);
-            clearIntroView();
-            loadResultView(data);
-        }
-    });
-}
-
-function getLocationSuccessFunction(position)
-{
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
-    console.log(lat, long);
-}
-
-function clearIntroView() {
-    $(".intro-text").hide('slow');
-    $(".begin-button").hide('slow');
-    $(".location-form").hide('slow');
-}
-
-function loadResultView(data) {
-    // todo: unpack other values; show weather, load wardrobe from backend
+function populateContent(data) {
     console.log(data);
-    var location = data.location;
-    var temp = data.temp;
-    var hum = data.humidity;
-    var pres = data.pressure;
-    var wind_speed = data.wind_speed;
-    var wind_deg = data.wind_deg;
-    $(".weather-text").text(location + ", " + temp + "°C, tuule kiirus " + wind_speed + " m/s");
+    for (i = 0; i < data.length; i++) {
+        var item = data[i];
+        var image = item.image;
+        var brand = item.brand;
+        var name = item.name;
+        var description = item.description;
+        $('.stock-content').append("" +
+            "<div class=\"card\">\n" +
+            "<img class=\"card-img-top\" src=\"" + image + "\" alt=\"Card image cap\"/>\n" +
+            "<div class=\"card-body\">\n" +
+            "<h5 class=\"card-title\">" + brand + " " + name + "</h5>\n" +
+            "<p class=\"card-text\">" + description + "</p>\n" +
+            "<a href=\"#\" class=\"btn btn-primary\">Detailid</a>\n" +
+            "</div>\n" +
+            "</div>");
+    }
+}
 
+function loadStock(){
+    $.ajax({
+        url: "/api/stock/",
+        success: function(data) {
+            data = $.parseJSON(data);
+            populateContent(data);
+        }
+    });
 }
 
 
 $(document).ready(function () {
-    // todo: request /api/stock, populate cards
-    // $(".intro-form").hide();
-    // if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(getLocationSuccessFunction, function (pos) {
-    //         $(".begin-button").hide();
-    //         $(".intro-form").show();
-    //     });
-    // } else {
-    //     alert('It seems like Geolocation, which is required for this page, is not enabled in your browser.');
-    // }
-    // $(".begin-button").click(function () {
-    //     loadWeatherData(lat, long);
-    // });
-    // $(".location-form").submit(function (event){
-    //     event.preventDefault();
-    //     loadWeatherDataByLocation($(".form-control").val());
-    // });
+    loadStock();
 });
 
