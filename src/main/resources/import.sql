5dba6f519ff185754c73494cefbe48440d58195a
Johannes Laamann <jelaamann@gmail.com>
2018-03-28 17:08:28 +0300

    Empty import.sql

--- src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-insert into stock_table (name, type, media_url) values ('adidas', 'sneakers', 'link to image')
-insert into stock_table (name, type, media_url) values ('zara', 't-shirt', 'link to image')
