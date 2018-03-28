18ab6f52907683673854fc9acf33ab8b9761e990
Johannes Laamann <jelaamann@gmail.com>
2018-03-28 17:56:31 +0300

    New MVP

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into stock_table (brand, name, type, price, color, image) values
-  ('adidas', 'stan smith', 'sneakers', 6000, 'white', 'https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg?sh=840&strip=false&sw=840');
+  ('adidas', 'stan smith', 'sneakers', 6000, 'white', 'https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
 insert into stock_table (brand, name, type, price, color, image) values
-  ('adidas', 'gazelles', 'sneakers', 6000, 'black', 'https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg?sh=600&strip=false&sw=600');
+  ('adidas', 'gazelles', 'sneakers', 6000, 'black', 'https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
