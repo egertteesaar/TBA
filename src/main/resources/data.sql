8a327378df9484348ac3971b1b4868ec0802d064
Johannes Laamann <jelaamann@gmail.com>
2018-03-28 18:47:58 +0300

    Add more inserts into data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,32 @@ insert into stock_table (brand, name, type, price, color, description, image) va
   ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
 insert into stock_table (brand, name, type, price, color, description, image) values
   ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
+-- duplikaadid (hiljem vahetada)
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
