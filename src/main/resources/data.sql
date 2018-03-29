155e09bc53803c6da93a684ba99c90199fd77638
Johannes Laamann <jelaamann@gmail.com>
2018-03-29 12:16:43 +0300

    Add more stuff to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,13 @@ insert into stock_table (brand, name, type, price, color, description, image) va
   ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
 insert into stock_table (brand, name, type, price, color, description, image) values
   ('adidas', 'gazelles', 'sneakers', 8000, 'black', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw44c162a4/zoom/BB5476_01_standard.jpg');
--- duplikaadid (hiljem vahetada)
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('new balance', '247', 'sneakers', 13000, 'blue', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://reede.ee/media/catalog/product/cache/3/thumbnail/315x470/9df78eab33525d08d6e5fb8d27136e95/n/e/new-balance-mrl247dm-1.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('dr. martens', '1460', 'boots', 18000, 'red', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://reede.ee/media/catalog/product/cache/3/thumbnail/315x470/9df78eab33525d08d6e5fb8d27136e95/d/r/drmartens-1460-vegan-cherry-red-1.jpg');
+insert into stock_table (brand, name, type, price, color, description, image) values
+  ('adidas', 'samba', 'sneakers', 11000, 'tan', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://reede.ee/media/catalog/product/cache/3/thumbnail/315x470/9df78eab33525d08d6e5fb8d27136e95/a/d/adidas-samba-fb-linen-1.jpg');
+-- duplikaadid
 insert into stock_table (brand, name, type, price, color, description, image) values
   ('adidas', 'stan smith', 'sneakers', 7500, 'white', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ','https://www.adidas.com/dis/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/dw1961a452/zoom/M20324_01_standard.jpg');
 insert into stock_table (brand, name, type, price, color, description, image) values
